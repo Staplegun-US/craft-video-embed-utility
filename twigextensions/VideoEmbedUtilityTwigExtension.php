e9492efd975ba561b61f847efc950fb4f4aa9bb2
ray hatfield <ray@staplegun.us>
2014-06-06 14:20:40 -0500

    Initial commit.

---
+++ twigextensions/VideoEmbedUtilityTwigExtension.php
@@ -0,0 +1,109 @@
+<?php
+	
+	namespace Craft;
+	
+	use Twig_Extension;
+	use Twig_Filter_Method;
+	
+	define("VIMEO",'vimeo.com');
+	define("YOUTUBE",'youtube.com');
+	
+	class VideoEmbedUtilityTwigExtension extends Twig_Extension {
+		
+		private static $KNOWN_HOSTS = array(VIMEO,YOUTUBE);
+		
+		public function getFilters() {
+			return array(
+				'videoPlayerUrl' => new Twig_Filter_Method($this,'videoPlayerUrl'),
+				'videoEmbed' => new Twig_Filter_Method($this,'videoEmbed'),
+				'videoHost' => new Twig_Filter_Method($this,'videoHost')
+			);
+		}
+		/**
+		 * Returns a string indicating where this video is hosted (youtube, vimeo, etc.)
+		 *
+		 * @param string $videoUrl
+		 * @return string
+		 */
+		public function videoHost($videoUrl) {
+			$host = parse_url($videoUrl, PHP_URL_HOST);
+			// return a sanitized value (no leading www, etc) if it's one we know.
+			foreach($this::$KNOWN_HOSTS as $known) {
+				if( strpos($videoUrl,$known) !== FALSE ) {
+					return $known;
+				}
+			}
+			return $host;
+		}
+				
+		public function videoId($videoUrl) {
+			$host = $this->videoHost($videoUrl);
+			switch($host) {
+				case VIMEO:
+					if(preg_match('/\/([0-9]+)\/*$/',$videoUrl,$matches) !== false) {
+						return $matches[1];
+					}
+				break;
+				
+				case YOUTUBE:
+					if(preg_match('/[&,v]=([^&]+)/',$videoUrl,$matches) !== false)
+						return $matches[1];
+					break;
+					
+				default:
+					throw new UnknownVideoHostException($videoUrl);				
+			}
+		}
+		
+		public function videoPlayerUrl($input) {
+			$vid = $this->videoId($input);
+			switch($this->videoHost($input)) {
+				case VIMEO:
+					return "//player.vimeo.com/video/$vid";
+				break;
+				
+				case YOUTUBE:
+					return "http://www.youtube.com/embed/$vid&controls=2";
+				break;
+				
+				default:
+				break;
+				
+			}
+			return "$vid - videoPlayerUrl-$input";
+		}
+		
+		/**
+		* Returns a boolean indicating whether the string $haystack ends with the string $needle.
+		* @param string $haystack the string to be searched
+		* @param string $needle the substring we're looking for
+		* return boolean
+		*/
+		private function endsWith($haystack, $needle) {
+			return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
+		}
+		
+		public function videoEmbed($input) {
+			$originalPath = craft()->path->getTemplatesPath();
+			$myPath = craft()->path->getPluginsPath() . 'videoembedutility/templates/';
+			craft()->path->setTemplatesPath($myPath);
+			$markup = craft()->templates->render('_vimeoEmbed.html', array(
+				'player_url' => $this->videoPlayerUrl($input)
+			));
+			craft()->path->setTemplatesPath($originalPath);
+			return TemplateHelper::getRaw($markup);
+		}
+		
+		public function getName() {
+			return 'Video Embed Utility Twig Extension';
+		}
+	}
+	
+	class UnknownVideoHostException extends Exception {
+		public function __construct($url) {
+			$this->url = $url;
+			parent::__construct('Unrecognized video host. Unable to extract id from url:' . $url);
+		}
+	}
+	
+?>
